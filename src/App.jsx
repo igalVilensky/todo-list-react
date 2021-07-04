264e05570fdcac4d5c408334c833bb870830a4cc
igalv <vilenskyigal@gmail.com>
2021-07-04 15:20:07 +0200

    structure, useState, handleSubmit, addTodos

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import "./app.scss";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false,
+    };
+
+    setTodos([...todos].concat(newTodo));
+    setTodo("");
+  }
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      {todos.map((todo) => (
+        <div>{todo.text}</div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
